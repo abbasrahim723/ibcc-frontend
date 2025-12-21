d796209c4285e88866875f255630225d89d54fc3
Abbas Rahim <abbasrahim723@gmail.com>
2025-12-22 02:24:23 +0500

    UI: hide header logo on mobile view

--- src/components/layout/header/HeaderLogo.vue
+++ src/components/layout/header/HeaderLogo.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link to="/" class="lg:hidden">
+  <router-link to="/" class="hidden">
     <img class="dark:hidden h-8 w-auto" src="/images/logo/logo.svg" alt="Logo" />
     <img class="hidden dark:block h-8 w-auto" src="/images/logo/logo-dark.svg" alt="Logo" />
   </router-link>
